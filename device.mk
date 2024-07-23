d1b1a457e19ad0d1b93d0b9d2d6bff4df89623f2
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-23 12:13:32 +0100

    fogos: Import Audio Configs  - From U1UG34.23-28-2
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- device.mk
+++ device.mk
@@ -11,6 +11,12 @@ $(call inherit-product, device/motorola/sm6375-common/sm6375.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Audio - Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
