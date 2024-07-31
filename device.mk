08c6914d289ee04fe6b44718ddc0303d8026ee71
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-31 09:19:17 +0100

    fogos: add init.recovery.fogos.rc
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- device.mk
+++ device.mk
@@ -6,3 +6,7 @@
 
 # Inherit from sm6375-common
 $(call inherit-product, device/motorola/sm6375-common/holi.mk)
+
+# recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.miami.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.miami.rc
