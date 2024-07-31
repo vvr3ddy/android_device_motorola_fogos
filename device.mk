7cc92fa42fba2799d270a08a5f0090c7afd1a63f
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-31 12:59:20 +0100

    fogos: Introduce Firmware for recovery root  - also try to load touch driver first to see if it helps fix touch screen in Recovery
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/motorola/sm6375-common/holi.mk)
 
 TARGET_NOT_USE_VENDOR_BOOT := true
 
-# recovery
+# Recovery
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.fogos.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.fogos.rc
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root/firmware/,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/)
