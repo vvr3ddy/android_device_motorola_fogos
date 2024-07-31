5e8bc134d7feb7901ab9e8a0b554244fbcded499
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-31 09:20:10 +0100

    fogos: Adapt to Android 14 Requirements for Modules in Recovery
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,5 +18,17 @@ TARGET_SCREEN_DENSITY := 420
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/fogos_defconfig
 
+# Kernel Modules - Vendor Boot
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+BOARD_GENERIC_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+RECOVERY_KERNEL_MODULES := $(BOARD_GENERIC_RAMDISK_KERNEL_MODULES_LOAD)
+
+# Kernel Modules - WLAN
+TARGET_MODULE_ALIASES += \
+    wlan.ko:qca_cld3_wlan.ko
+
 # Inherit from sm6375-common
 include device/motorola/sm6375-common/BoardConfigCommon.mk
--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from sm6375-common
 $(call inherit-product, device/motorola/sm6375-common/holi.mk)
 
+
+TARGET_NOT_USE_VENDOR_BOOT := true
+
 # recovery
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/init.recovery.miami.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.miami.rc
+    $(LOCAL_PATH)/recovery/root/init.recovery.fogos.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.fogos.rc
