fd5a8258cebcd9d245fc57deb7a02e05a54907c0
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-08-13 21:22:52 +0100

    fogos: Import device manifest and compat matrix
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,18 @@ TARGET_FS_CONFIG_GEN := \
     $(COMMON_PATH)/mot_aids.fs
 
 # HIDL
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+
+DEVICE_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml \
+    hardware/qcom-caf/common/compatibility_matrix.xml
+
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/device_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Init
 define copy_files
