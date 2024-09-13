db9274a832360438548e652bee59132446a10e82
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-09-13 22:57:31 +0100

    fogos: Drop duplicate declaration of common files
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,3 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
-
-# Inherit from sm6375-common
-include device/motorola/sm6375-common/BoardConfigCommon.mk
